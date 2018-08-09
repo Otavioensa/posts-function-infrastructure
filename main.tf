04ebd06dd508b94d666267b3ed063a8cde5a6682
Otavioensa <otavioensa@gmail.com>
2018-08-08 22:36:35 -0300

    aws-provider: add aws provider
    add aws provider

--- main.tf
+++ main.tf
@@ -2,22 +2,3 @@ provider "aws" {
   profile = "standard-aws-credentials"
   region  = "us-east-1"
 }
-
-resource "aws_dynamodb_table" "application_table" {
-  name           = "${var.tableName}-data"
-  read_capacity  = 5
-  write_capacity = 1
-
-  hash_key = "day"
-
-  attribute {
-    name = "day"
-    type = "S"
-  }
-
-  tags {
-    Name    = "${var.tableName}"
-    Project = "${var.project}"
-    Env     = "staging"
-  }
-}
