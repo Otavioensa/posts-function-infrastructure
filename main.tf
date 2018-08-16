c7c6528efe06aa145e88a8a977153f7b2e686fe4
Otavioensa <otavioensa@gmail.com>
2018-08-15 23:01:48 -0300

    dynamo: add table for locks on tfstate
    add table for when .tdtate is locked

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ provider "aws" {
   profile = "standard-aws-credentials"
   region  = "us-east-1"
 }
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
